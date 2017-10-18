a210719effca698abfdfb8800d0968bf3939218f
Marius <jorgensen.marius@gmail.com>
2017-10-18 14:01:22 +0100

    Sulfuras will never change quality from 80

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,11 +1,12 @@
 require 'gilded_rose'
+require 'pry'
 
 describe 'Aged Brie' do
   it 'increases in Quality as it matures' do
     aged_brie = Item.new('Aged Brie', 30, 1)
     gilded_rose = GildedRose.new([aged_brie])
 
-    expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 1
+    expect { gilded_rose.update_quality }.to change { aged_brie.quality }
   end
 
   context 'when the expire date hits' do
@@ -33,5 +34,14 @@ describe 'Aged Brie' do
 
       expect { gilded_rose.update_quality }.not_to change { aged_brie.quality }.from 50
     end
+  end
+end
+
+describe 'Sulfuras, Hand of Ragnaros' do
+  it 'will never decrease or increase in quality' do
+    sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 10, 80)
+    gilded_rose = GildedRose.new([sulfuras])
+
+    expect { gilded_rose.update_quality }.not_to change { sulfuras.quality }
   end
 end
