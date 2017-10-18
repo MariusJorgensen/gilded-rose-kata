13073085d844b4c1fb353df2b27dc181b5a7f8de
Marius <jorgensen.marius@gmail.com>
2017-10-18 12:48:47 +0100

    removed failing hookup test

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,11 +1,5 @@
 require 'gilded_rose'
 
-describe '2+2' do
-  it 'equals 4' do
-    expect(2+2).to eq 4
-  end
-end
-
 describe 'Aged Brie' do
   it 'increases in Quality as it matures' do
     aged_brie = Item.new('Aged Brie', 30, 1)
@@ -31,5 +25,5 @@ describe 'Aged Brie' do
       expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
     end
   end
-  
+
 end
