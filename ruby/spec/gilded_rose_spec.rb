dd42489bfdc3cca8d6cd311c1063cc9ee501ca49
Marius <jorgensen.marius@gmail.com>
2017-10-18 12:39:26 +0100

    two tests passing

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -7,15 +7,19 @@ describe '2+2' do
 end
 
 describe 'Aged Brie' do
-
   it 'increases in Quality as it matures' do
-
     aged_brie = Item.new('Aged Brie', 30, 1)
-
     gilded_rose = GildedRose.new([aged_brie])
 
     expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 1
-
   end
 
+  context 'when the expire date hits' do
+    it 'increases in quality by 2' do
+      aged_brie = Item.new('Aged Brie', 0, 1)
+      gilded_rose = GildedRose.new([aged_brie])
+
+      expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
+    end
+  end
 end
