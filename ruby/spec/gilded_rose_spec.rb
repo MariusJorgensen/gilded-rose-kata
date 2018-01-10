6b50f4038be246c2debe9a107595029b535e861e
Marius <jorgensen.marius@gmail.com>
2018-01-10 09:22:31 +0000

    Added new test for update sell by date + removed duplication

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,40 +1,48 @@
+require 'spec_helper'
 require 'gilded_rose'
 require 'pry'
 
 describe 'Aged Brie' do
   it 'increases in Quality as it matures' do
     aged_brie = Item.new('Aged Brie', 30, 1)
-    gilded_rose = GildedRose.new([aged_brie])
 
-    expect { gilded_rose.update_quality }.to change { aged_brie.quality }
+    expect { update_quality_of(aged_brie)}.to change { aged_brie.quality }
   end
 
   context 'when the expire date hits' do
     it 'increases in quality by 2' do
       aged_brie = Item.new('Aged Brie', 0, 1)
-      gilded_rose = GildedRose.new([aged_brie])
 
-      expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
+      expect { update_quality_of(aged_brie) }.to change { aged_brie.quality }.by 2
     end
   end
 
   context 'when the expire date has passed' do
     it 'increases in quality by 2' do
       aged_brie = Item.new('Aged Brie', -1, 1)
-      gilded_rose = GildedRose.new([aged_brie])
 
-      expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
+      expect { update_quality_of(aged_brie) }.to change { aged_brie.quality }.by 2
     end
   end
 
   context 'when the quality is 50' do
     it 'does not increase in quality' do
       aged_brie = Item.new('Aged Brie', 1, 50)
-      gilded_rose = GildedRose.new([aged_brie])
 
-      expect { gilded_rose.update_quality }.not_to change { aged_brie.quality }.from 50
+      expect { update_quality_of(aged_brie) }.not_to change { aged_brie.quality }.from 50
     end
   end
+
+  it 'reduces the days left to sell it by 1' do
+    aged_brie = Item.new('Aged Brie', 10, 10)
+
+    expect { update_quality_of(aged_brie) }.to change { aged_brie.sell_in }.by -1
+  end
+end
+
+def update_quality_of(aged_brie)
+  gilded_rose = GildedRose.new([aged_brie])
+  gilded_rose.update_quality
 end
 
 describe 'Sulfuras, Hand of Ragnaros' do
