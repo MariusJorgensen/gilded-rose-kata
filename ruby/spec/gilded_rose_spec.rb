baf5e816c268d3309fc8e85ec024c12f3d67b34a
Marius <jorgensen.marius@gmail.com>
2018-01-10 09:31:16 +0000

    Removed duplication and made method reusable

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -40,41 +40,32 @@ describe 'Aged Brie' do
   end
 end
 
-def update_quality_of(aged_brie)
-  gilded_rose = GildedRose.new([aged_brie])
-  gilded_rose.update_quality
-end
-
 describe 'Sulfuras, Hand of Ragnaros' do
   it 'will never decrease or increase in quality' do
     sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 10, 80)
-    gilded_rose = GildedRose.new([sulfuras])
 
-    expect { gilded_rose.update_quality }.not_to change { sulfuras.quality }
+    expect { update_quality_of(sulfuras) }.not_to change { sulfuras.quality }
   end
 end
 
 describe 'Normal Item' do
   it 'decreases quality when it gets older' do
     normal_item = Item.new('Normal Item', 10, 10)
-    gilded_rose = GildedRose.new([normal_item])
 
-    expect { gilded_rose.update_quality }.to change { normal_item.quality }.by -1
+    expect { update_quality_of(normal_item) }.to change { normal_item.quality }.by -1
   end
 
   it 'will never have a quality less than 0' do
     normal_item = Item.new('Normal Item', 10, 0)
-    gilded_rose = GildedRose.new([normal_item])
 
-    expect { gilded_rose.update_quality }.not_to change { normal_item.quality }
+    expect { update_quality_of(normal_item) }.not_to change { normal_item.quality }
   end
 
   context 'when the sell by day has passed' do
     it 'decrease quality twice as fast' do
       normal_item = Item.new('Normal Item', 0, 50)
-      gilded_rose = GildedRose.new([normal_item])
 
-      expect { gilded_rose.update_quality }.to change { normal_item.quality }.by -2
+      expect { update_quality_of(normal_item) }.to change { normal_item.quality }.by -2
     end
   end
 end
@@ -83,27 +74,29 @@ describe 'Backstage passes' do
   context 'when the sell by date is between 10 and 6 days away' do
     it 'increases in quality twice as fast' do
       backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)
-      gilded_rose = GildedRose.new([backstage_pass])
 
-      expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 2
+      expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 2
     end
   end
 
   context 'when the sell by date is between 5 and 0 days away' do
     it 'increases in quality three times as fast' do
       backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)
-      gilded_rose = GildedRose.new([backstage_pass])
 
-      expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 3
+      expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 3
     end
   end
 
   context 'when the sell by date has been hit' do
     it 'drop the quality to 0' do
       backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)
-      gilded_rose = GildedRose.new([backstage_pass])
 
-      expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.to 0
+      expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.to 0
     end
   end
+end
+
+def update_quality_of(item)
+  gilded_rose = GildedRose.new([item])
+  gilded_rose.update_quality
 end
