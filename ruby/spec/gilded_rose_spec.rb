e1652fa13ee7fb838f94654a0fabe1cde96c2a9f
Marius <jorgensen.marius@gmail.com>
2018-03-09 09:14:29 +0000

    refactored hard coded strings

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -79,20 +79,20 @@ describe 'Normal Item' do
   end
 
   it 'decreases quality when it gets older' do
-    normal_item = Item.new('Normal Item', 10, 10)
+    normal_item = normal_item(sell_in: 10, quality: 10)
 
     expect { update_quality_of(normal_item) }.to change { normal_item.quality }.by -1
   end
 
   it 'will never have a quality less than 0' do
-    normal_item = Item.new('Normal Item', 10, 0)
+    normal_item = normal_item(sell_in: 10, quality: 0)
 
     expect { update_quality_of(normal_item) }.not_to change { normal_item.quality }
   end
 
   context 'when the sell by day has passed' do
     it 'decrease quality twice as fast' do
-      normal_item = Item.new('Normal Item', 0, 50)
+      normal_item = normal_item(sell_in: 0, quality: 50)
 
       expect { update_quality_of(normal_item) }.to change { normal_item.quality }.by -2
     end
@@ -101,7 +101,7 @@ describe 'Normal Item' do
   context 'when it has expired' do
     context 'and the quality is already 0' do
       it 'does not change in quality' do
-        normal_item = Item.new('Normal Item', -1, 0)
+        normal_item = normal_item(sell_in: -1, quality: 0)
 
         expect { update_quality_of(normal_item) }.not_to change { normal_item.quality }.from 0
       end
@@ -116,7 +116,7 @@ end
 describe 'Backstage passes' do
   context 'when the sell by date is between 10 and 6 days away' do
     it 'increases in quality twice as fast' do
-      backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)
+      backstage_pass = backstage_pass(sell_in: 10, quality: 10)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 2
     end
@@ -124,7 +124,7 @@ describe 'Backstage passes' do
 
   context 'when the sell by date is between 5 and 0 days away' do
     it 'increases in quality three times as fast' do
-      backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)
+      backstage_pass = backstage_pass(sell_in: 5, quality: 10)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 3
     end
@@ -132,28 +132,28 @@ describe 'Backstage passes' do
 
   context 'when the sell by date has been hit' do
     it 'drop the quality to 0' do
-      backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+      backstage_pass = backstage_pass(sell_in: 0, quality: 10)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.to 0
     end
   end
 
   it 'reduces the days left to sell it by 1' do
-    backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)
+    backstage_pass = backstage_pass(sell_in: 10, quality: 10)
 
     expect { update_quality_of(backstage_pass) }.to change { backstage_pass.sell_in }.by -1
   end
 
   context 'when the concert has finished' do
     it 'has no quality' do
-      backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 10)
+      backstage_pass = backstage_pass(sell_in: -1, quality: 10)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.to 0
     end
 
     context 'and the quality is already 0' do
       it 'should not change from 0' do
-        backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 0)
+        backstage_pass = backstage_pass(sell_in: -1, quality: 0)
 
         expect { update_quality_of(backstage_pass) }.to_not change { backstage_pass.quality }.from 0
       end
@@ -162,7 +162,7 @@ describe 'Backstage passes' do
 
   context 'when there are precisely 11 days left to sell' do
     it 'increases quality by 1' do
-      backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 3)
+      backstage_pass = backstage_pass(sell_in: 11, quality: 3)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 1
     end
@@ -170,10 +170,14 @@ describe 'Backstage passes' do
 
   context 'when there are precisely 6 days left to sell' do
     it 'increases quality by 2' do
-      backstage_pass = Item.new('Backstage passes to a TAFKAL80ETC concert', 6, 3)
+      backstage_pass = backstage_pass(sell_in: 6, quality: 3)
 
       expect { update_quality_of(backstage_pass) }.to change { backstage_pass.quality }.by 2
     end
+  end
+
+  def backstage_pass(sell_in:, quality:)
+    Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in, quality)
   end
 end
 
